5dbb117f921e0128fa073b2651c1e27dbde04618
ciisiq <cii_3@hotmail.com>
2022-03-06 11:38:59 +0000

    73 creating Event Listner

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number!🎉 ';
+//Selecting
+// document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//Events listners
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+  }
+});
